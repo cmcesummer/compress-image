c901031269c71b5ce0ab2cfff90bd65cb30f93d3
zhanghao05 <cmcesummer@126.com>
2019-12-12 16:33:52 +0800

    更新远程自动压缩

--- src/tiny.js
+++ src/tiny.js
@@ -17,8 +17,6 @@ function upload({ imgPath, cb = () => {}, retryTime = 2 }) {
                 url: "https://tinypng.com/web/shrink",
                 method: "POST",
                 headers: {
-                    rejectUnauthorized: false,
-                    "Postman-Token": Date.now(),
                     "Cache-Control": "no-cache",
                     "Content-Type": "application/x-www-form-urlencoded",
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
